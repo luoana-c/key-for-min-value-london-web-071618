bbcf0b52fba320c105c0a9591e864fd77f468bcc
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-05 05:53:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  value_array = []
+  name_hash.collect do |key, value|
+    value_array.push(value)
+  end
+  value_array.collect do |value1, value2|
+    if value1 > value2
 end
